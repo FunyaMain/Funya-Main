8e3db7650ee7b17c61ef2a61c75fa7056c8922a5
FunyaMain <funyamain@gmail.com>
2026-04-24 21:00:42 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,15 @@
+# config.py
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
+
+TOPIC_BUG = int(os.getenv("TOPIC_BUG"))
+TOPIC_ERROR = int(os.getenv("TOPIC_ERROR"))
+TOPIC_REPORT = int(os.getenv("TOPIC_REPORT"))
+TOPIC_OTHER = int(os.getenv("TOPIC_OTHER"))
+TOPIC_USERS = int(os.getenv("TOPIC_USERS"))
