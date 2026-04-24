dced7fee9f0884927943863fe4114051bc6c3ba0
FunyaMain <funyamain@gmail.com>
2026-04-24 21:02:29 +0300

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,1 +1,21 @@
+# services/database.py
+import asyncpg
+from config import DATABASE_URL
 
+pool = None
+
+async def connect():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+async def execute(query, *args):
+    async with pool.acquire() as conn:
+        return await conn.execute(query, *args)
+
+async def fetch(query, *args):
+    async with pool.acquire() as conn:
+        return await conn.fetch(query, *args)
+
+async def fetchrow(query, *args):
+    async with pool.acquire() as conn:
+        return await conn.fetchrow(query, *args)
