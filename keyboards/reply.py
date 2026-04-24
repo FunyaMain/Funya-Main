d7e0613478e4b7f21dfab2080ea792cd86323ce0
FunyaMain <funyamain@gmail.com>
2026-04-24 21:02:57 +0300

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,1 +1,11 @@
+# keyboards/reply.py
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def profile_kb():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="🔄 Обмен"), KeyboardButton(text="⛏ Копать")],
+            [KeyboardButton(text="💧 Полить"), KeyboardButton(text="✋ Фап")]
+        ],
+        resize_keyboard=True
+    )
